e4c5d532ffb95a3fd72a3ec3fad06d04141874c5
Alexandru Grigore <alex.grigore@boulevard.rentals>
2018-01-08 01:58:00 +0200

    CSS fixes

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -3,7 +3,7 @@
     <h1>Under Construction</h1>
     <div>
       <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:cosmin.simion@boulevard.rentals">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
-      <p>&mdash; Boulevard Rentals Team</p>
+      <p>&mdash; Boulevard Rentals &copy; Team</p>
     </div>
   </div>
   
@@ -19,16 +19,4 @@ export default {
   }
 }
 </script>
-
-<style>
-#app{
-    body { text-align: center; padding: 150px; }
-    h1 { font-size: 50px; }
-    body { font: 20px Helvetica, sans-serif; color: #333; }
-    article { display: block; text-align: left; width: 650px; margin: 0 auto; }
-    a { color: #dc8100; text-decoration: none; }
-    a:hover { color: #333; text-decoration: none; }
-    }
-</style>
-
 
