80cebfe1ca6d4346ad5fff123d6bbf344773981b
Cosmin Simion (RINF) <a-cosimi@microsoft.com>
2018-01-08 14:55:00 +0200

    Adding buttons on the landing page with the first views we need

--- src/components/landingPage.vue
+++ src/components/landingPage.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
+    <div class="waves-effect waves-light btn">Sign up</div>
+    <div class="waves-effect waves-light btn">Sign in</div>
+    <div class="waves-effect waves-light btn">Search</div>
+    <div class="waves-effect waves-light btn">Add a new location</div>
     <h1>Under Construction</h1>
     <div>
       <p>Sorry for the inconvenience but we&rsquo;re performing some maintenance at the moment. If you need to you can always <a href="mailto:cosmin.simion@boulevard.rentals">contact us</a>, otherwise we&rsquo;ll be back online shortly!</p>
